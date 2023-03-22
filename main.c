89d039f2d52ef3e394ce93d12e01b349c5b301c3
GBSBRE001 <111292614+GBSBRE001@users.noreply.github.com>
2023-03-22 14:11:26 +0200

    Q2.1) init_external_interrupts Function
    Q2.1) init_external_interrupts Function

--- main.c
+++ main.c
@@ -20,48 +20,66 @@ void init_LEDs(void);
 void display_on_LEDs(uint8_t count);
 void delay(unsigned int microseconds);
 void init_switches(void);
+void InitExternalInterrupts(void);
+void EXTI2_3_IRQHandler(void);
 
 
 // MAIN FUNCTION -------------------------------------------------------------|
 
 uint8_t count = 0;
+uint8_t display_on = 0;
 
 void main(void)
 {
 	init_LEDs();
     init_LCD();
     init_switches();
+    InitExternalInterrupts();
 
 
     while(1)
     {
-    	display_on_LEDs(count);
-    	display_on_LCD(count);
-
-        if ((( GPIOA -> IDR & GPIO_IDR_1) == 0)){
-            if( count < 255){
+        if (((GPIOA->IDR & GPIO_IDR_1) == 0) && display_on)
+        {
+            if (count < 255)
+            {
                 count++;
             }
             display_on_LEDs(count);
             display_on_LCD(count);
             delay(50000);
         }
-        else if((( GPIOA -> IDR & GPIO_IDR_2) == 0)){
-            if(count <= 0){
-                count = 0;
-            }
-            else{
+        else if (((GPIOA->IDR & GPIO_IDR_2) == 0) && display_on)
+        {
+            if (count > 0)
+            {
                 count--;
             }
-            display_on_LCD(count);
             display_on_LEDs(count);
+            display_on_LCD(count);
+            delay(50000);
+        }
+        else if ((GPIOA->IDR & GPIO_IDR_3) == 0)
+        {
+            if (display_on)
+            {
+                // turn off display and LEDs
+                display_on = 0;
+                GPIOB->ODR = 0;
+                lcd_command(CLEAR);
+            }
+            else
+            {
+                // turn on display and LEDs
+            	count = 0;
+                display_on = 1;
+                display_on_LEDs(count);
+                display_on_LCD(count);
+            }
             delay(50000);
         }
     }
-
 }
-
-
 
 
 // OTHER FUNCTIONS -----------------------------------------------------------|
@@ -106,9 +124,41 @@ void init_switches(void)
 
     GPIOA -> MODER &= ~GPIO_MODER_MODER1;
     GPIOA -> MODER &= ~GPIO_MODER_MODER2;
+    GPIOA -> MODER &= ~GPIO_MODER_MODER3;
 
     GPIOA -> PUPDR |= GPIO_PUPDR_PUPDR1_0;
     GPIOA -> PUPDR |= GPIO_PUPDR_PUPDR2_0;
+    GPIOA -> PUPDR |= GPIO_PUPDR_PUPDR3_0;
 
 }
 /* -----------------------------------------------------------*/
+void InitExternalInterrupts(void){
+
+    RCC -> APB2ENR |= RCC_APB2ENR_SYSCFGCOMPEN;
+
+    SYSCFG -> EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PA;
+    SYSCFG -> EXTICR[0] |= SYSCFG_EXTICR1_EXTI1_PA;
+    SYSCFG -> EXTICR[0] |= SYSCFG_EXTICR1_EXTI3_PA;  // Set EXTI3 to PA3
+
+    EXTI->IMR |= EXTI_IMR_MR3;
+    EXTI->FTSR |= EXTI_FTSR_TR3;
+
+    NVIC_EnableIRQ(EXTI2_3_IRQn);
+
+}
+
+/* -----------------------------------------------------------*/
+void EXTI2_3_IRQHandler(void)
+{
+	EXTI->PR &= EXTI_PR_PR3;
+
+}
+/* -----------------------------------------------------------*/
+
+
+
+
+
+
+
+
